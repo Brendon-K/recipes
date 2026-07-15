9f70488ecff045a4b2c6dc4e74f5bcd69a4418e5
Brendon Kieser <brendon.kieser@gmail.com>
2026-07-14 18:15:09 -0700

    correcting url and key

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,5 +2,6 @@
 //const BACKEND_URL = "http://127.0.0.1:8000";
 // Live
 const BACKEND_URL = "https://recipes-dil8.onrender.com";
-const SUPABASE_URL = "https://jayysbkqgqcxkofsilym.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_YAV2w_0VJoZkFHgFGkWTSQ_zVa0q8-J";
+
+const SUPABASE_URL = "https://wzedbxujkxzhgfaocpuw.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_AFRLDE_lcy2hxe3w8TI1zQ_qQ2maqr2";
